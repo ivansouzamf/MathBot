eec15b296d52741e0844c0ee59dfa20907949600
Payn <ivanttt54@gmail.com>
2025-12-02 04:41:01 -0300

    minor changes

--- math.go
+++ math.go
@@ -8,49 +8,44 @@ import (
 )
 
 func EvaluateMathExp(exp string) (float64, error) {
-	InvalidExp := errors.New("Invalid Expression")
-
 	tokens, err := TokenizeMathExp(exp)
 	if err != nil {
-		return 0.0, InvalidExp
+		return 0.0, ErrInvalidExp
 	}
 
 	res, _, err := ParseMathTokens(tokens, 0, false)
 	if err != nil {
-		return 0.0, InvalidExp
+		return 0.0, ErrInvalidExp
 	}
 
 	return res, nil
 }
 
 func ParseMathTokens(tokens []Token, i int, isRecursive bool) (float64, int, error) {
-	InvalidSyntax := errors.New("Invalid Syntex")
 	acumulator := 0.0
 
 	for i = i; i < len(tokens); i += 1 {
 		previous := Token{Invalid, None, 0.0}
-		if i != 0 {
-			previous = tokens[i - 1]
-		}
+		if i > 0 { previous = tokens[i - 1] }
 
-		// Check for invalid Syntax, like double operator and no operator between numbers
-		if previous.kind == tokens[i].kind || previous.kind >= Plus && tokens[i].kind >= Plus {
-			return 0.0, 0, InvalidSyntax
+		// Check for invalid Syntax
+		// E.g. Double operator or no operator between numbers
+		if previous.kind == tokens[i].kind ||
+		   previous.kind >= Plus && tokens[i].kind >= Plus {
+			return 0.0, 0, errInvalidSyntax
 		}
 
 		if tokens[i].kind == Number {
 			acumulator += tokens[i].num
 			continue
 		}
-		
+
 		j := i
 		right := tokens[i + 1].num
 
 		currentPrec := tokens[i].prec
 		nextPrec := Low
-		if i + 1 < len(tokens) - 1 {
-			nextPrec = tokens[i + 2].prec
-		}
+		if i + 1 < len(tokens) - 1 { nextPrec = tokens[i + 2].prec }
 
 		// If precedence increases we recurse
 		recurse := currentPrec < nextPrec
@@ -61,7 +56,7 @@ func ParseMathTokens(tokens []Token, i int, isRecursive bool) (float64, int, err
 				return 0.0, 0, err
 			}
 		}
-		
+
 		switch tokens[i].kind {
 			case Plus: acumulator += right
 			case Minus: acumulator -= right
@@ -69,7 +64,7 @@ func ParseMathTokens(tokens []Token, i int, isRecursive bool) (float64, int, err
 			case Divi: acumulator /= right
 			case Powe: acumulator = math.Pow(acumulator, right)
 		}
-		
+
 		if j != i {
 			i = j
 		} else if recurse || nextPrec < currentPrec && isRecursive {
@@ -84,12 +79,11 @@ func ParseMathTokens(tokens []Token, i int, isRecursive bool) (float64, int, err
 }
 
 func TokenizeMathExp(exp string) ([]Token, error) {
-	InvalidToken := errors.New("Invalid token")
 	tokens := []Token{}
 
 	for i := 0; i < len(exp); i += 1 {
-		nullToken := Token{Invalid, None, 0.0}
-		token := nullToken
+		invalidToken := Token{Invalid, None, 0.0}
+		token := invalidToken
 		switch exp[i] {
 			case ' ': continue
 			case '(': token = Token{Open, VeryHigh, 0.0}
@@ -97,19 +91,17 @@ func TokenizeMathExp(exp string) ([]Token, error) {
 			case '+': token = Token{Plus, Low, 0.0}
 			case '-': {
 				last := Token{Plus, None, 0.0}
-				if len(tokens) != 0 {
-					last = tokens[len(tokens) - 1]
-				}
+				if len(tokens) > 0 { last = tokens[len(tokens) - 1] }
 
 				if last.kind < Plus {
 					token = Token{Minus, Low, 0.0}
 				}
-			}	
+			}
 			case '*': token = Token{Multi, High, 0.0}
 			case '/': token = Token{Divi, High, 0.0}
 			case '^': token = Token{Powe, VeryHigh, 0.0}
 		}
-		if token != nullToken {
+		if token != invalidToken {
 			tokens = append(tokens, token)
 			continue
 		}
@@ -118,25 +110,27 @@ func TokenizeMathExp(exp string) ([]Token, error) {
 		for j := i; true; j += 1 {
 			isPossibleNum := false
 			if j < len(exp) {
-				isPossibleNum = unicode.IsDigit(rune(exp[j])) || exp[j] == '.' || exp[j] == '-'
+				isPossibleNum = unicode.IsDigit(rune(exp[j])) ||
+				                exp[j] == '.'                 ||
+				                exp[j] == '-'
 			}
 
 			if isPossibleNum {
 				numStr += string(exp[j])
 				continue
 			}
-			
+
 			// NOTE: This actually handles all the possible errors of the tokenizer
-			// since everything that's not valid is treated as a possible number and 
+			// since everything that's not valid is treated as a possible number and
 			// then checked here
 			numF64, err := strconv.ParseFloat(numStr, 64)
 			if err != nil {
-				return nil, InvalidToken
+				return nil, errInvalidToken
 			}
 
-			tokens = append(tokens, Token{Number, None, numF64})
-			
 			i += (j - i) - 1
+			tokens = append(tokens, Token{Number, None, numF64})
+
 			break
 		}
 	}
@@ -152,7 +146,7 @@ type Token struct {
 
 type TokenKind uint32
 const (
-	Invalid TokenKind = iota 
+	Invalid TokenKind = iota
 	Number
 	Open
 	Close
@@ -169,4 +163,12 @@ const (
 	Low
 	High
 	VeryHigh
+)
+
+var (
+	// Internal errors
+	errInvalidToken  error = errors.New("Invalid Token")
+	errInvalidSyntax error = errors.New("Invalid Syntax")
+	// Public errors
+	ErrInvalidExp error = errors.New("Invalid Expression")
 )
